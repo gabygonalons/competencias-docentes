fc3cd82df115e05bf32e93c0a00172c722a4b3f0
Gabriela Beatriz Goñalons <119463275+gabygonalons@users.noreply.github.com>
2024-10-22 12:29:30 -0300

    Add files via upload

---
+++ Procesamiento.py
@@ -0,0 +1,44 @@
+import streamlit as st
+
+# Inserción de la imágen del encabezado
+st.image("encabezado.png")
+
+texto = """
+# B I E N V E N I D O S
+-----------------------------------------
+El uso y apropiación de las Tecnologías de la Información y la Comunicación (TIC) al servicio del mejoramiento de la calidad y equidad de la educación, y la competitividad profesional requiere de procesos formativos continuos y eficaces. 
+Un correcto diagnóstico de las Competencias Digitales Docentes permitirá el establecimiento de procesos de formación continuos, coherentes, escalonados y lógicos que garanticen un desarrollo profesional docente acorde a las necesidades actuales propias del rol docente y de las disciplinas específicas.
+
+## Definición de Competencia
+
+A los efectos de la investigación se define competencia como “el conjunto de conocimientos, habilidades, actitudes, comprensiones y disposiciones cognitivas, socioafectivas y psicomotoras apropiadamente relacionadas entre sí para facilitar el desempeño flexible, eficaz y con sentido de una actividad en contextos relativamente nuevos y retadores”; tomado de las Competencias TIC para el desarrollo profesional docente colombiano, Ministerio de Educación Nacional de Colombia, publicado en 2006. Bajo Licencia Creative Commons - Atribución No Comercial.
+
+## Pentágono de Competencias TIC
+
+La elección del Pentágono de Competencias TIC desarrollado en Colombia obedece a su simplicidad y su orientación hacia la construcción de trayectos formativos. Permite establecer Niveles de Competencias o Momentos dado que las competencias se desarrollan y expresan en diferentes niveles o grados de complejidad. 
+
+- **Primer Nivel - Exploración:** Permite el acercamiento a un conjunto de conocimientos que se constituyen en la posibilidad para acceder a estados de mayor elaboración conceptual.
+
+- **Segundo Nivel - Integración:** Plantea el uso de los conocimientos ya apropiados para la resolución de problemas en contextos diversos.
+
+- **Tercer Nivel - Innovación:** Da mayor énfasis a los ejercicios de creación; lo que permite ir más allá del conocimiento aprendido e imaginar nuevas posibilidades de acción o explicación.
+
+## Competencias incluidas en el Pentágono
+
+1. Tecnológicas
+2. Pedagógicas
+3. Comunicativas
+4. De Gestión
+5. Investigativas
+
+"""
+st.markdown(texto)
+
+# Inserción de la imágen del pentágono
+st.image("pentagono_cdd.png")
+
+st.markdown('***')
+
+
+
+
